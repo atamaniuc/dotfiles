fe0bddad91cf95f6726083e36b0e0522df7d2efe
Na1k0n <d.atamaniuc@gmail.com>
2018-02-17 15:58:07 +0200

    Update alert.fish

--- home/.config/fish/functions/alert.fish
+++ home/.config/fish/functions/alert.fish
@@ -1,6 +1,3 @@
 function alert
-	if contains -- --sms $argv[1]
-	echo 'Job is done with a status of ' $status ' @' (date)| twilio-sms 903-354-5247 > /dev/null 
-        end
         paplay ~/.config/fish/functions/TaDa.ogg
 end
