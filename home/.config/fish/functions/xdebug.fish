6642768372cfa9d13f0aa7dd1387c2f7a29802e5
Na1k0n <d.atamaniuc@gmail.com>
2019-02-28 19:21:37 +0200

    Update xdebug.fish

--- home/.config/fish/functions/xdebug.fish
+++ home/.config/fish/functions/xdebug.fish
@@ -1,9 +1,11 @@
 function xdebug
 	switch $argv[1]
 	case 'on'
-            set --export --global XDEBUG_CONFIG 'idekey=phpstorm'
+	    sudo phpenmod xdebug
+            set --export --global XDEBUG_CONFIG 'idekey=PHPSTORM'
             echo Xdebug activated
 	case 'off'
+	    sudo phpdismod xdebug
             set --erase XDEBUG_CONFIG
 	    echo Xdebug deactivated
     end
